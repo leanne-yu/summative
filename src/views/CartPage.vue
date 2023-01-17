78874d90a7632ac5ba6017e74f2becf4f586f933
[leanne-yu] <[leanneyu3299@gmail.com]>
2023-01-17 01:44:05 -0500

    Created remove button, but need to fix.

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -3,14 +3,16 @@ import { useStore } from "../store/index.js"
 import SiteHeader from "../components/SiteHeader.vue";
 
 const store = useStore();
-console.log(store.cart);
+
 </script>
 
 <template>
   <SiteHeader />
   <div v-for="movie in Array.from(store.cart.values())">
     <img :src="`https://image.tmdb.org/t/p/w500${movie.poster}`" />
-    <h1>{{movie.title}}</h1>
+    <h1>{{ movie.title }}</h1>
+    <h1>{{ movie.date }}</h1>
+    <button @click="store.removeFromCart(movie.id)">Remove</button>
   </div>
 </template>
 
